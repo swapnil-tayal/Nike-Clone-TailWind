3cfb64f5be0560526fb53bf707d2b1ca8a188676
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2023-09-10 00:07:28 +0530

    Add files via upload

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import SuperQuality from './SuperQuality'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import CustomerReview from './CustomerReview'
+import Subscribe from './Subscribe'
+import Fotter from './Fotter'
+
+export{
+    Hero,
+    Subscribe,
+    CustomerReview,
+    SpecialOffer,
+    Services,
+    SuperQuality,
+    PopularProducts,
+    Fotter
+}
